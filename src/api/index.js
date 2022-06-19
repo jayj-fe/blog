9905391f01e6f0d0387826fa7254f925ad694403
gitplus <gitplus@witplus.com>
2022-06-19 23:57:57 +0900

    test

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 export default axios.create({
   withCredentials: true,
-  // baseURL: 'https://jayj-fe.github.io/',
-  baseURL: 'http://localhost:9000/',
+  baseURL: 'https://jayj-fe.github.io/',
+  // baseURL: 'http://localhost:9000/',
   headers :{
     post: {
       'Content-Type' : 'application/json;charset=utf-8',
